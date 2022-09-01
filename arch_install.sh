cdc54657d3c3a29b319d8eca17dbf9bc39b8a0ec
krolyxon <krolyxon@tutanota.com>
2022-09-01 21:48:33 +0530

    keyd-git

--- arch_install.sh
+++ arch_install.sh
@@ -108,7 +108,7 @@ git clone --depth=1 git@github.com:krolyxon/nvim.git ~/.config/nvim
 
 # paru: AUR helper
 git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si && cd .. && rm -rf paru-bin
-paru -S --noconfirm htop-vim nerd-fonts-jetbrains-mono lf-bin libxft-bgra zsh-fast-syntax-highlighting-git
+paru -S --noconfirm htop-vim nerd-fonts-jetbrains-mono lf-bin libxft-bgra zsh-fast-syntax-highlighting-git keyd-git
 chsh -s $(which zsh)
 printf "bro do you want to install that fucking nvidia-390xx drivers? (y/n): " 
 read driver
